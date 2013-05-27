c9b712f65bcc59a37aebacf40e240244d3573980
andunslg <andunslg@gmail.com>
2013-05-27 12:10:07 +0530

    Adding the sample

---
+++ src/main/java/com/googlecode/wicket/jquery/ui/plugins/wysiwyg/sample/HomePage.java
@@ -0,0 +1,16 @@
+package com.googlecode.wicket.jquery.ui.plugins.wysiwyg.sample;
+
+import com.googlecode.wicket.jquery.ui.plugins.wysiwyg.WysiwygBehavior;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+		this.add(new WysiwygBehavior("#editor","wysiwyg"));
+
+	}
+}
