b8e58c5da4999699e380ed5399fc367f55b3f8a9
andunslg <andunslg@gmail.com>
2013-06-01 00:23:26 +0530

    Testing

--- src/main/java/com/googlecode/wicket/jquery/ui/plugins/wysiwyg/sample/HomePage.java
+++ src/main/java/com/googlecode/wicket/jquery/ui/plugins/wysiwyg/sample/HomePage.java
@@ -1,8 +1,11 @@
 package com.googlecode.wicket.jquery.ui.plugins.wysiwyg.sample;
 
 import com.googlecode.wicket.jquery.ui.WysiwygEditor;
+import com.googlecode.wicket.jquery.ui.WysiwygText;
 import com.googlecode.wicket.jquery.ui.plugins.wysiwyg.WysiwygBehavior;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 
@@ -11,7 +14,20 @@ public class HomePage extends WebPage{
 
 	public HomePage(final PageParameters parameters){
 		super(parameters);
+
+		final WysiwygEditor wysiwygEditor=new WysiwygEditor("container",new Model<WysiwygText>());
+
+		Form form=null;
+		add(form=new Form("form"){
+			@Override
+			protected void onSubmit(){
+				WysiwygText wysiwygText=(WysiwygText)wysiwygEditor.getDefaultModelObject();
+				info(wysiwygText.getText());
+			}
+		});
+		form.add(wysiwygEditor);
 		this.add(new WysiwygBehavior("#editorArea","wysiwyg"));
-		this.add(new WysiwygEditor("buttonPanel","editorArea"));
+//		this.add(new WysiwygEditor("buttonPanel","editorArea"));
+
 	}
 }
