d65114dcd1af6c89fd0fbd133ec615d8ae54d2e2
andunslg <andunslg@gmail.com>
2013-06-05 23:43:21 +0530

    Changing the sample to use the new WysiwygDefaultButtonToolbar

--- src/main/java/com/googlecode/wicket/jquery/ui/plugins/wysiwyg/sample/HomePage.java
+++ src/main/java/com/googlecode/wicket/jquery/ui/plugins/wysiwyg/sample/HomePage.java
@@ -1,11 +1,10 @@
 package com.googlecode.wicket.jquery.ui.plugins.wysiwyg.sample;
 
-import com.googlecode.wicket.jquery.ui.WysiwygEditor;
-import com.googlecode.wicket.jquery.ui.WysiwygText;
-import com.googlecode.wicket.jquery.ui.plugins.wysiwyg.WysiwygBehavior;
+
+import com.googlecode.wicket.jquery.ui.plugins.wysiwyg.WysiwygDefaultToolbar;
+import com.googlecode.wicket.jquery.ui.plugins.wysiwyg.WysiwygEditor;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
-import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.Model;
@@ -14,11 +13,27 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage{
 	private static final long serialVersionUID=1L;
+	String value="Type";
+	WysiwygEditor wysiwygEditor;
+	WysiwygDefaultToolbar wysiwygDefaultToolbar;
 
 	public HomePage(final PageParameters parameters){
 		super(parameters);
 
-		final WysiwygEditor wysiwygEditor=new WysiwygEditor("editorContainer",new Model<WysiwygText>());
-		this.add(wysiwygEditor);
+		Form form=new Form("form");
+		form.add(new AjaxButton("submitBtn"){
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form){
+				System.out.println(wysiwygEditor.getModelObject());
+				System.out.println(value);
+			}
+
+		}) ;
+
+		wysiwygDefaultToolbar=new WysiwygDefaultToolbar("toolbarContainer");
+		form.add(wysiwygDefaultToolbar);
+		wysiwygEditor=new WysiwygEditor("editorContainer",new Model<String>(value),wysiwygDefaultToolbar);
+		form.add(wysiwygEditor);
+		this.add(form);
 	}
 }
